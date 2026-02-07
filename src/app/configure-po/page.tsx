3990512cd0ec182bdc70d35ac9b2bb88f3798c27
Ethan Palosh <109639295+epalosh@users.noreply.github.com>
2026-02-07 00:01:26 -0800

    try 1

--- src/app/configure-po/page.tsx
+++ src/app/configure-po/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 "use client";
+export const dynamic = "force-dynamic";
 
 import { Suspense } from "react";
 import ConfigurePOClient from "./ConfigurePOClient";
